232e8c40adf7fb3f793e0a1ec1044c2d9e82714e
wtj2732 <wtj2732@163.com>
2026-06-02 20:56:00 +0800

    Update code

--- run_commands.sh
+++ run_commands.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
-set -e
+
 
 python train.py \
   --config configs/datasets/cifar10/cifar10.yml \
   configs/networks/resnet18_32x32.yml \
   configs/pipelines/train/train_sem.yml \
   configs/preprocessors/base_preprocessor.yml \
-  --num_workers 12 \
   --network.checkpoint ./results/pre-train/best.ckpt
 
 python test.py \
@@ -16,5 +15,4 @@ python test.py \
   configs/pipelines/test/test_fsood.yml \
   configs/preprocessors/base_preprocessor.yml \
   configs/postprocessors/gmm.yml \
-  --num_workers 8 \
   --mark 0
